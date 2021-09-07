d186fd1ea6ac6735cd08aa1b02f2050d92dbf81e
Victor <64934876+VictorMargheriti@users.noreply.github.com>
2021-09-06 23:07:13 -0400

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            name: 'Victor',
+            fullName: 'Victor Margheriti'
+        }
+    },
+    methods: {
+    }
+})
+
+app.mount("#portfolio");
